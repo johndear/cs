4f305d9f85b5516b51a05d885d7879e86037a330
johndear <ls123456>
2016-04-18 22:49:59 +0800

    customer

---
+++ public/common/ws_service.js
@@ -0,0 +1,65 @@
+/**
+ * 意见反馈websocket服务 Created by 廖金洪
+ */
+'use strict';
+
+chatApp.service('WebSocketService', [ '$timeout','$rootScope','$http', function($timeout,$rootScope,$http) {
+	var self = this;
+	var ws = null;
+	
+	// liusu temp
+	var socketServerAddress= 'localhost';
+	var socketServerPort = '8887';
+
+	function newWebSocket() {
+		var url = "ws://"+socketServerAddress + ":" + socketServerPort + '?customerId=' + 123;
+		var wsTmp = new WebSocket(url);
+		wsTmp.onopen = function(evnt) {
+			onOpen(evnt);
+		};
+		wsTmp.onmessage = function(evnt) {
+			onMessage(evnt);
+		};
+		wsTmp.onclose = function(evnt) {
+			onClose(evnt);
+		};
+		wsTmp.onerror = function(evnt) {
+			onError(evnt);
+		};
+		return wsTmp;
+	}
+
+	ws = newWebSocket();
+
+	function onOpen(evnt) {
+		console.log("onOpen: ", evnt);
+	}
+
+	function onClose(evnt) {
+		console.log("onClose: ", evnt);
+
+//		$timeout(function() {
+//			console.log('Reconnecting to server...')
+//			newWebSocket();
+//		}, 3000);
+	}
+
+	function onMessage(evnt) {
+		console.log("onMessage: ", evnt);
+		// 这里处理接收数据
+		var evenData = evnt.data;
+		console.log("意见反馈websocket服务端推送数据: ", evenData);
+
+		// 传回后端实时推送的数据
+		$rootScope.$broadcast('ws-user-msg', evenData);
+	}
+
+	function onError(evnt) {
+		console.log('onError: ', evnt);
+//		$timeout(function() {
+//			console.log('Reconnecting to server...')
+//			newWebSocket();
+//		}, 3000);
+	}
+
+} ]);
