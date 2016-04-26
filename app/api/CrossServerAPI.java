d292a9ac318fe26206e253d3af647720789d59bf
johndear <ls123456>
2016-04-26 08:17:15 +0800

    send

---
+++ app/api/CrossServerAPI.java
@@ -0,0 +1,23 @@
+package api;
+
+public class CrossServerAPI {
+	
+	public void sendToUser(Long userId, String message){
+		
+	}
+	
+	public void sendToCustomer(Long customerId, String message){
+		
+	}
+	
+	// 用户发给客服
+	public void receiveFromUser(Long userId, String message){
+		
+	}
+
+	// 客服发给用户
+	public void receiveFromCustomer(Long customerId, String message){
+		
+	}
+
+}
