2c87782e569ef1cc7d892713fdffa8c3c96c6d8c
liusu <wb-liusu.a@alibaba-inc.com>
2016-09-26 10:11:02 +0800

    crud

--- module/crud/app/models/BaseModel.java
+++ module/crud/app/models/BaseModel.java
@@ -8,23 +8,29 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
+import controllers.CRUD.Exclude;
 import play.db.jpa.Model;
 
 @MappedSuperclass
 public class BaseModel extends Model{
 
+	@Exclude
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name="create_time")
 	public Date createTime;
 	
+	@Exclude
 	public String createby;
 	
+	@Exclude
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name="modify_time")
 	public Date modifyTime;
 	
+	@Exclude
 	public String modifyby;
 
+	@Exclude
 	public int deleted;
 	
 	@Transient
