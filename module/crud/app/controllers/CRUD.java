e3db725c5a19e94154ecb7f87cf3423fd2723b87
liusu <wb-liusu.a@alibaba-inc.com>
2017-01-03 17:21:09 +0800

    crud

--- module/crud/app/controllers/CRUD.java
+++ module/crud/app/controllers/CRUD.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -21,10 +20,17 @@ import java.util.Map.Entry;
 
 import javax.persistence.Transient;
 
-import models.TResource;
-
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 
+import annotation.Action;
+import annotation.ListColumn;
+import annotation.Menu;
+import annotation.QueryParam;
+import annotation.TableExclude;
+import controllers.CRUD.ObjectType.ObjectField;
+import models.ActionBean;
+import models.TResource;
 import play.Logger;
 import play.Play;
 import play.classloading.enhancers.ControllersEnhancer.ControllerInstrumentation;
@@ -34,23 +40,17 @@ import play.data.validation.Password;
 import play.data.validation.Required;
 import play.db.Model;
 import play.db.Model.Factory;
+import play.db.Model.Property;
+import play.db.jpa.JPAPlugin.JPAModelLoader;
 import play.exceptions.TemplateNotFoundException;
 import play.mvc.ActionInvoker;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.Router;
 import play.utils.Java;
-import services.ResourceService;
 import utils.ExcelUtil;
 import utils.Position;
 import utils.ReflectUtils;
-import annotation.Action;
-import annotation.ListColumn;
-import annotation.Menu;
-import annotation.QueryParam;
-import annotation.TableExclude;
-import controllers.CRUD.ObjectType;
-import controllers.CRUD.ObjectType.ObjectField;
 
 public abstract class CRUD extends Controller {
 	
@@ -86,21 +86,20 @@ public abstract class CRUD extends Controller {
     }
 
     public static void list(int page, String search, String searchFields, String orderBy, String order) {
-    	// liusu start
     	try{
-//	    	_list(page, search, searchFields, orderBy, order);
-    		
-    		// 防止302跳转
     		ControllerInstrumentation.initActionCall();
     		Method method = CRUD.class.getDeclaredMethod("_list", new Class[]{int.class, String.class, String.class,String.class,String.class});
 			ActionInvoker.invokeControllerMethod(method, new Object[]{page, search, searchFields, orderBy, order});
+			
+//	    	_list(page, search, searchFields, orderBy, order);
+	    	render();
         } catch (Exception e) {
         	ControllerInstrumentation.stopActionCall();
+            render("CRUD/list.html");
         }
-    	render("CRUD/list.html");
     }
-    
-    public static void _list(int page, String search, String searchFields, String orderBy, String order) throws Exception{
+
+	public static void _list(int page, String search, String searchFields, String orderBy, String order) throws Exception{
 		Map<String, String[]> objectParams = params.getRootParamNode().originalParams;
     	String where = "";
     	for (Entry entry : objectParams.entrySet()) {
@@ -126,10 +125,15 @@ public abstract class CRUD extends Controller {
         	List<String> searchFieldArr = new ArrayList<String>();
         	Field[] fields = type.entityClass.getFields();
         	for (Field field : fields) {
-        		Transient transientAnnotation = field.getAnnotation(Transient.class);
-        		if(transientAnnotation != null && !"innerTableAction".equals(field.getName())){
-        			continue;
+        		QueryParam queryParam = field.getAnnotation(QueryParam.class);
+        		if(queryParam!=null){
+        			searchFieldArr.add(field.getName());
         		}
+        		
+//        		Transient transientAnnotation = field.getAnnotation(Transient.class);
+//        		if(transientAnnotation != null && !"innerTableAction".equals(field.getName())){
+//        			continue;
+//        		}
         		TableExclude tableExclude = field.getAnnotation(TableExclude.class);
         		if(tableExclude==null){
         			ListColumn listColumn = field.getAnnotation(ListColumn.class);	
@@ -142,10 +146,6 @@ public abstract class CRUD extends Controller {
         				listFieldArr.add(field.getName());
         			}
         		}
-        		QueryParam queryParam = field.getAnnotation(QueryParam.class);
-        		if(queryParam!=null){
-        			searchFieldArr.add(field.getName());
-        		}
 			}
         	
         	if(listFieldArr.contains("id")){
@@ -157,25 +157,28 @@ public abstract class CRUD extends Controller {
         	
         	Class controllerClass = getControllerClass();
         	Method[] methods = ReflectUtils.getBeanPublicStaticMethods(controllerClass);
-        	Map<String, String> outerTableAction = null;
-        	Map<String, String> innerTableAction = null;
+        	Map<String, ActionBean> outerTableAction = null;// (Map<String, String>) type.entityClass.getField("outerTableAction").get(type.entityClass);
+        	Map<String, ActionBean> innerTableAction = null;// (Map<String, String>) type.entityClass.getField("innerTableAction").get(type.entityClass);;
         	for (Method method : methods) {
-        		if(",addType,index,layout,redirect,list,attachment,save,create,exportExcel,_list,layout2,".contains(","+method.getName()+",")){
-        			continue;
-        		}
+//        		if(",addType,index,layout,redirect,list,attachment,save,create,exportExcel,_list,layout2,blank,".contains(","+method.getName()+",")){
+//        			continue;
+//        		}
         		if(outerTableAction==null){
-        			outerTableAction = new HashMap<String, String>();
+        			outerTableAction = new HashMap<String, ActionBean>();
         		}
         		if(innerTableAction==null){
-        			innerTableAction = new HashMap<String, String>();
+        			innerTableAction = new HashMap<String, ActionBean>();
         		}
         		Action action = method.getAnnotation(Action.class);
-        		String actionUrl = Router.getFullUrl(controllerClass.getName()+"."+method.getName());
         		if(action!=null){
+        			ActionBean abean = new ActionBean();
+        			abean.code = action.code();
+        			abean.name = action.name();
+        			abean.url = Router.getFullUrl(controllerClass.getName()+"."+method.getName());
         			if(Position.INNER == action.position()){
-        				innerTableAction.put(action.name(), actionUrl);
+        				innerTableAction.put(action.code(), abean);
         			}else{
-        				outerTableAction.put(action!=null ? action.name() : method.getName(), actionUrl);
+        				outerTableAction.put(action.code(), abean);
         			}
         		}else{
         			// 没有使用action注解方法，默认不展示
@@ -218,7 +221,125 @@ public abstract class CRUD extends Controller {
         	throw new Exception();
         }
 	}
+	
+	public static void no_result_list(int page, String search, String searchFields, String orderBy, String order) throws Exception{
+		Map<String, String[]> objectParams = params.getRootParamNode().originalParams;
+    	String where = "";
+    	for (Entry entry : objectParams.entrySet()) {
+    		String key = (String) entry.getKey();
+    		String val = ((String[])entry.getValue())[0];
+    		if(key.contains("object.") && StringUtils.isNotEmpty(val)){
+    			where +=  key.replace("object.", "") + " like '%" + val + "%' and ";
+    		}
+    	}
+    	where = StringUtils.isNotEmpty(where)? where.substring(0, where.lastIndexOf("and")) : null;
+    	// liusu end
+    	
+        ObjectType type = ObjectType.get(getControllerClass());
+        notFoundIfNull(type);
+        if (page < 1) {
+            page = 1;
+        }
+//        List<Model> objects = type.findPage(page, search, searchFields, orderBy, order, where);
+//        Long count = type.count(search, searchFields, where);
+//        Long totalCount = type.count(null, null, where);
+        try {
+        	List<String> listFieldArr = new ArrayList<String>();
+        	List<String> searchFieldArr = new ArrayList<String>();
+        	Field[] fields = type.entityClass.getFields();
+        	for (Field field : fields) {
+        		QueryParam queryParam = field.getAnnotation(QueryParam.class);
+        		if(queryParam!=null){
+        			searchFieldArr.add(field.getName());
+        		}
+        		
+//        		Transient transientAnnotation = field.getAnnotation(Transient.class);
+//        		if(transientAnnotation != null && !"innerTableAction".equals(field.getName())){
+//        			continue;
+//        		}
+        		TableExclude tableExclude = field.getAnnotation(TableExclude.class);
+        		if(tableExclude==null){
+        			ListColumn listColumn = field.getAnnotation(ListColumn.class);	
+        			if(listColumn!=null){
+        				String[] fieldNames = listColumn.fields().split(",");
+        				for (String fieldName : fieldNames) {
+        					listFieldArr.add(field.getName() +"."+ fieldName);
+						}
+        			}else{
+        				listFieldArr.add(field.getName());
+        			}
+        		}
+			}
+        	
+        	if(listFieldArr.contains("id")){
+        		listFieldArr.remove("id");
+        	}
+        	if(listFieldArr.contains("willBeSaved")){
+        		listFieldArr.remove("willBeSaved");
+        	}
+        	
+        	Class controllerClass = getControllerClass();
+        	Method[] methods = ReflectUtils.getBeanPublicStaticMethods(controllerClass);
+        	Map<String, ActionBean> outerTableAction = null;// (Map<String, String>) type.entityClass.getField("outerTableAction").get(type.entityClass);
+        	Map<String, ActionBean> innerTableAction = null;// (Map<String, String>) type.entityClass.getField("innerTableAction").get(type.entityClass);;
+        	for (Method method : methods) {
+//        		if(",addType,index,layout,redirect,list,attachment,save,create,exportExcel,_list,layout2,blank,".contains(","+method.getName()+",")){
+//        			continue;
+//        		}
+        		if(outerTableAction==null){
+        			outerTableAction = new HashMap<String, ActionBean>();
+        		}
+        		if(innerTableAction==null){
+        			innerTableAction = new HashMap<String, ActionBean>();
+        		}
+        		Action action = method.getAnnotation(Action.class);
+        		if(action!=null){
+        			ActionBean abean = new ActionBean();
+        			abean.code = action.code();
+        			abean.name = action.name();
+        			abean.url = Router.getFullUrl(controllerClass.getName()+"."+method.getName());
+        			if(Position.INNER == action.position()){
+        				innerTableAction.put(action.code(), abean);
+        			}else{
+        				outerTableAction.put(action.code(), abean);
+        			}
+        		}else{
+        			// 没有使用action注解方法，默认不展示
+        		}
+			}
+        	
+        	// 只有当有行内操作时，才显示
+        	if(innerTableAction==null || innerTableAction.size()==0){
+        		listFieldArr.remove("innerTableAction");
+        	}
+        	
+        	Menu menu = getControllerClass().getAnnotation(Menu.class);
+        	String menuCategory = "default";
+        	if(StringUtils.isNotEmpty(menu.category())){
+        		menuCategory = menu.category();
+        	}
+        	boolean isNew = newCrud;
+        	
+        	renderArgs.put("type", type);
+        	renderArgs.put("page", page);
+        	renderArgs.put("orderBy", orderBy);
+        	renderArgs.put("order", order);
+        	renderArgs.put("searchFieldArr", searchFieldArr);
+        	renderArgs.put("listFieldArr", listFieldArr);
+        	renderArgs.put("innerTableAction", innerTableAction);
+        	renderArgs.put("outerTableAction", outerTableAction);
+        	renderArgs.put("isNew", isNew);
+        	renderArgs.put("menuCategory", menuCategory);
+        } catch (TemplateNotFoundException e) {
+        	renderArgs.put("type", type);
+        	renderArgs.put("page", page);
+        	renderArgs.put("orderBy", orderBy);
+        	renderArgs.put("order", order);
+        	throw new Exception();
+        }
+	}
     
+	@Action(code="exportExcel", name="导出", position=Position.OUTER)
     public static void exportExcel(Integer page, String search, String searchFields, String orderBy, String order) {
 //    	String where = (String) request.args.get("where");
 //    	String where = params.get("where");
@@ -414,7 +535,7 @@ public abstract class CRUD extends Controller {
     public @interface Hidden {}
 
     // ~~~~~~~~~~~~~
-    static int getPageSize() {
+    public static int getPageSize() {
         return Integer.parseInt(Play.configuration.getProperty("crud.pageSize", "5"));
     }
 
@@ -434,8 +555,12 @@ public abstract class CRUD extends Controller {
         public ObjectType(Class<? extends Model> modelClass) {
             this.modelName = modelClass.getSimpleName();
             this.entityClass = modelClass;
-            this.factory = Model.Manager.factoryFor(entityClass);
-            this.keyName = factory.keyName();
+            try{
+            	this.factory = Model.Manager.factoryFor(entityClass);
+            	this.keyName = factory.keyName();
+            }catch(Exception e){
+            	Logger.info("%s 不是一个model", entityClass);
+            }
         }
 
         @SuppressWarnings("unchecked")
@@ -529,22 +654,46 @@ public abstract class CRUD extends Controller {
         public List<ObjectField> getFields() {
             List<ObjectField> fields = new ArrayList<ObjectField>();
             List<ObjectField> hiddenFields = new ArrayList<ObjectField>();
-            for (Model.Property f : factory.listProperties()) {
-                ObjectField of = new ObjectField(f);
-                if (of.type != null) {
-                    if (of.type.equals("hidden")) {
-                        hiddenFields.add(of);
-                    } else {
-                        fields.add(of);
-                    }
-                }
+            
+            List<Model.Property> props = null;
+            if(factory!=null){
+            	props = factory.listProperties();
+            }else{
+            	props = new JPAModelLoader(this.entityClass).listProperties();
             }
-
+            
+            for (Model.Property f : props) {
+        		ObjectField of = new ObjectField(f);
+        		if (of.type != null) {
+        			if (of.type.equals("hidden")) {
+        				hiddenFields.add(of);
+        			} else {
+        				fields.add(of);
+        			}
+        		}
+        	}
             hiddenFields.addAll(fields);
+            
             return hiddenFields;
         }
 
         public ObjectField getField(String name) {
+			try {
+				Field transtField = this.entityClass.getField(name);
+				Transient transt = transtField.getAnnotation(Transient.class);
+				if(transt!=null){
+					Model.Property property = new Property();
+					property.field = transtField;
+					property.name = transtField.getName();
+					property.type = transtField.getType();
+					return new ObjectField(property);
+				}
+			} catch (NoSuchFieldException e) {
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			}
+        	
             for (ObjectField field : getFields()) {
                 if (field.name.equals(name)) {
                     return field;
@@ -553,28 +702,28 @@ public abstract class CRUD extends Controller {
             return null;
         }
 
-        @Override
+//        @Override
         public int compareTo(ObjectType other) {
             return String.valueOf(this.orderNo).compareTo(String.valueOf(other.orderNo));
         }
         
         public static int getOrderNo(ObjectType other){
-        	Class<? extends Model> otherEntityClass =other.entityClass;
-            CRUD.For otherForan = other.controllerClass.getAnnotation(CRUD.For.class);
-            Menu menu = other.controllerClass.getAnnotation(Menu.class);
-            String otherMenuCode = StringUtils.isNotEmpty(menu.code()) ? menu.code() : (otherForan==null ? otherEntityClass.getSimpleName():otherForan.value().getSimpleName());
-//            TResource tr = TResource.find("code=?", otherMenuCode).first();
+//        	Class<? extends Model> otherEntityClass =other.entityClass;
+//            CRUD.For otherForan = other.controllerClass.getAnnotation(CRUD.For.class);
+//            Menu menu = other.controllerClass.getAnnotation(Menu.class);
+//            String otherMenuCode = StringUtils.isNotEmpty(menu.code()) ? menu.code() : (otherForan==null ? otherEntityClass.getSimpleName():otherForan.value().getSimpleName());
+////            TResource tr = TResource.find("code=?", otherMenuCode).first();
             int orderNo  = 0;
-			try {
-	            List<Map<String, String>> list;
-				list = ResourceService.query(otherMenuCode);
-				if(list!=null && list.size()>0){
-					orderNo = Integer.parseInt(list.get(0).get("orderNo"));
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+//			try {
+//	            List<Map<String, String>> list;
+//				list = ResourceService.query(otherMenuCode);
+//				if(list!=null && list.size()>0){
+//					orderNo = Integer.parseInt(list.get(0).get("orderNo"));
+//				}
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
             return orderNo;
         }
 
@@ -651,5 +800,21 @@ public abstract class CRUD extends Controller {
             }
         }
     }
+    
+    /**
+     * <p> getParameter: 从request获取参数  </p>
+     * <p> 多个value用","分隔</p>
+     * @param key
+     * @return
+     */
+    public static String getParameter(String key) {
+		Map<String, String[]> objectParams = params.getRootParamNode().originalParams;
+		String[] values = objectParams.get(String.format("object.%s", key));
+		if (ArrayUtils.isEmpty(values) || values.length <= 0) {
+			return null;
+		}
+		return StringUtils.trim(StringUtils.join(values, ","));
+	}
+    
 }
 
---
+++ module/crud/app/models/ActionBean.java
@@ -0,0 +1,15 @@
+package models;
+
+import utils.Position;
+
+public class ActionBean {
+	
+	//接口名称
+    public String code;
+	//接口名称
+    public String name;
+    
+    public String url;
+    
+
+}
